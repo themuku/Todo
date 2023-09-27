3ba5c151c342e45000608363da7e353eef5f44b1
themuku <murad.mesteliyev3@gmail.com>
2023-09-27 06:37:58 -0700

    Cors is not realised fully

---
+++ config/corsOptions.js
@@ -0,0 +1,14 @@
+const whitelist = ["http://localhost:3000", "https://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS!"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+export default corsOptions;
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import connectDb from "./config/dbConnection.js";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import todoRoutes from "./routes/todoRoutes.js";
 import authMiddleware from "./middleware/authMiddleware.js";
+import corsOptions from "./config/corsOptions.js";
 
 dotenv.config();
 
@@ -20,6 +22,9 @@ const port = process.env.SERVER_PORT;
 
 // Creating app with express
 const app = express();
+
+// Cross Origin Resource Sharing - CORS Middleware
+app.use(cors(corsOptions));
 
 // Body parser middleware for enabling req.body which returns parsed json
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }));
